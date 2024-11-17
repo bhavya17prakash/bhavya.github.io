2520bb393f81445479431542d09c6aaf615a3c9a
Bhavya Prakash <prakash.b@northeastern.edu>
2024-11-16 19:56:18 -0500

    Export :bug: fix

--- utils/clickAnanlytics.ts
+++ utils/clickAnanlytics.ts
@@ -1,12 +1,12 @@
 // import { Analytics, logEvent } from "firebase/analytics";
 // import { analytics } from "./firebase";
 
-// export const CONTACT_BTN = "contact_btn_clicked";
-// export const SOCIAL_BTN = "_social_btn_clicked";
-// export const PROJECT_CLICKED = "_project_clicked";
-// export const EMAIL_CLICKED = "email_btn_clicked";
-// export const CALL_BTN_CLICKED = "call_btn_clicked";
-// export const RESUME_BTN = "resume_btn_clicked";
+export const CONTACT_BTN = "contact_btn_clicked";
+export const SOCIAL_BTN = "_social_btn_clicked";
+export const PROJECT_CLICKED = "_project_clicked";
+export const EMAIL_CLICKED = "email_btn_clicked";
+export const CALL_BTN_CLICKED = "call_btn_clicked";
+export const RESUME_BTN = "resume_btn_clicked";
 
 export const analyseBtnClicked = (event: string) => {
   // _logEvent(analytics, event);
